a259f8e06283dae9212e0051b61c297654c38789
Kay Holten <holtenkay@gmail.com>
2020-09-22 20:23:32 -0700

    home page wireframe

---
+++ index.js
@@ -0,0 +1,8 @@
+let recipeHTML = `<li class="recipe-group">
+    <div class="recipe-item">
+        <h3>Recipe title</h3>
+        <span>time minutes / number ingredients / cuisine</span>
+        <p>Description</p>
+    </div>
+    <div class="square"></div>
+</li>`;
