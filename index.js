51177d5fe8a1de12cb39a2c6de5b27756fea35ca
Kay Holten <holtenkay@gmail.com>
2020-10-05 15:43:00 -0700

    trying to fix population and button function

--- index.js
+++ index.js
@@ -54,8 +54,6 @@ function formatIngredients(responseJson, i) {
     }
 }
 
-let destination = `/just-recipe.html`
-
 function displayRecipes(responseJson) {
     console.log(responseJson);
     //if there are results, remove them
@@ -70,13 +68,13 @@ function displayRecipes(responseJson) {
             <ol class="ingr-list-${i} i-my-ingrs">${
                 responseJson.hits[i].recipe.ingredientLines.map(item =>
                     `<li>${item}</li>`).join("")}</ol>
-            <form action="hungry-but-lazy/just-recipe.html" id="i-lets-make-it">
+            <form id="i-lets-make-it">
+                <input id="test" type="submit" value="Let's make it!"/>
                 <input type="hidden" name="title" value="${responseJson.hits[i].recipe.label}">
                 <input type="hidden" name="ingredients" value="${responseJson.hits[i].recipe.ingredientLines}">
                 <input type="hidden" name="src" value="${responseJson.hits[i].recipe.image}">
                 <input type="hidden" name="num" value="${responseJson.hits[i].recipe.ingredientLines.length}">
                 <input type="hidden" name="link" value="${responseJson.hits[i].recipe.url}">
-                <input id="test" type="submit" value="Let's make it!"/>
             </form>
         </div>
         <div class="square"><img src="${responseJson.hits[i].recipe.image}" alt="recipe image"/></div>
@@ -119,7 +117,7 @@ function unhappyResult() {
 }
 
 //When form on index page is submitted, do this
-function watchIndexForm() {
+function watchLetsEat() {
     $('#i-form').submit(event => {
         event.preventDefault();
         console.log('watchIndexForm ran');
@@ -135,64 +133,94 @@ function watchIndexForm() {
 
 function watchLetsMakeIt() {
     $('#i-recipes').on('submit', event => {
-        console.log('watchLetsMakeIt ran');
-    })
-}
-
-function populateJRPage() {
-    const [title, ingredients, src, num, link] = decodeURIComponent(window.location.search.substr(1)
-            .replaceAll("+", " "))
-            .split('&')
-            .map(part => part.split("=")[1])
-    const prettyIngr = ingredients.replaceAll(',');
-    $('.jr-recipe-title').html(`${title}`);
-    $('.jr-ingr-num').html(`${num} ingredients`);
-    $('.jr-recipe-img').html(`<img src="${src}" alt="recipe image">`);
-    $('.jr-recipe-box').append(`<iframe id="jr-recipe"
-    title="Your recipe"
-    width="100%"
-    height="800px"
-    src="${link}"></iframe>`)
-}
-
-//BonAPI stuff
-
-function getSubs() {
-    console.log('getSubs ran')
-    let subUrl = "https://www.bon-api.com/api/v1/ingredient/alternatives/whole_cow_milk/";
-    let token = "Token ec69b072c20a1d404b390bc1afa21f0b15b196d4";
-
-    const options = {
-        headers: new Headers({
-            "Authorization": token})
-    };
-
-    fetch(subUrl, options)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(response.statusText);
-        })
-        .then(responseJson => console.log(responseJson));
-}
-
-function watchSubSubmit() {
-    $('.jr-sub-station').on('submit', event => {
         event.preventDefault();
-        console.log('watchSubSubmit ran')
-        let request = $('.jr-sub-request').val();
-        getSubs();
+        console.log('watchLetsMakeIt ran');
+        $(".everything").empty();
+        let title = $(event.currentTarget).closest("#i-lets-make-it").find("input[name=title]").val();
+        let src = $("input[name=src]").val();
+        let num = $("input[name=num]").val();
+        let link = $("input[name=link]").val();
+        holdRecipeHTML(src, title, num, link);
     })
 }
 
+// function populateJRPage() {
+//     // const [title, ingredients, src, num, link] = decodeURIComponent(window.location.search.substr(1)
+//     //         .replaceAll("+", " "))
+//     //         .split('&')
+//     //         .map(part => part.split("=")[1])
+//     // $('.jr-recipe-title').html(`${title}`);
+//     // $('.jr-ingr-num').html(`${num} ingredients`);
+//     // $('.jr-recipe-img').html(`<img src="${src}" alt="recipe image">`);
+//     // $('.jr-recipe-box').append(`<iframe id="jr-recipe"
+//     // title="Your recipe"
+//     // width="100%"
+//     // height="800px"
+//     // src="${link}"></iframe>`)
+//     let title = $(this).closest("form").find("input[name=title]").val();
+//     let src = $("input[name=src]").val();
+//     let num = $("input[name=num]").val();
+//     let link = $("input[name=link]").val();
+//     holdRecipeHTML(src, title, num, link);
+// }
 
+function holdRecipeHTML(src, title, num, link) {
+    const html= `<div class="jr-group">
+        <div class="jr-item">
+            <div class="jr-recipe-img">
+                <img src="${src}" alt="recipe image">
+            </div>
+            <div>
+                <h2>${title}</h2>
+                <span>${num} ingredients</span>
+            </div>
+        </div>
+        <section class="jr-breakouts"> 
+            <div class="jr-sub-station">
+                <h2>Sub station</h2>
+                <p>Don't feel like going to the grocery store? Find some alternatives!</p>
+                <iframe width="100%" height="400" src="https://bon-api.com/iframe/1"></iframe>
+            </div>
+            <div class="jr-recipe-ingredients">
+                <h2>Sub log</h2>
+                <form>
+                    <label for="have-not">I'm swapping </label><input type="text" name="have-not" id="have-not">
+                    <label for="have">for </label><input type="text" name="have" id="have">.
+                    <input type="submit" class="add-sub" value="Add">
+                </form>
+                <ul id="jr-sub-log"></ul>
+            </div>
+        </section>
+    </div>
+    <section>
+        <div class="jr-recipe-box">
+            <iframe id="jr-recipe"
+                title="Your recipe"
+                width="100%"
+                height="800px"
+                src="${link}">
+            </iframe>
+        </div>
+    </section>`
+    $('.everything').empty();
+    $(".everything").append(html);
+}
+
+function watchSubLogSubmit() {
+    $(".everything").on("submit", ".add-sub", function(event) {
+        event.preventDefault();
+        console.log('watchSubLogSubmit ran');
+        // const haveNot = $("#have-not").val();
+        // const have = $("#have").val();
+        // console.log(haveNot);
+        // console.log(have);
+    })
+}
 
 function onPageLoad() {
-    watchIndexForm();
+    watchLetsEat();
     watchLetsMakeIt();
-    populateJRPage();
-    watchSubSubmit();
+    watchSubLogSubmit();
 }
 
 $(onPageLoad)
